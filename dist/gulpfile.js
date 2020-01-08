4d85de151f9fdcb93fe1bd01b2e9f5901c50574c
archist <hi.cyril@gmail.com>
2020-01-08 19:17:01 +0100

    to john

---
+++ dist/gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require("gulp")
+const bs = require("browser-sync").create()
+const jshint = require("gulp-jshint")
+const uglify = require("gulp-uglify")
+const jade = require("gulp-jade")
+// const babel = require("gulp-babel")
+// const babel = require("babel")
+
+
+function _html(){
+  return gulp.src("*.html")
+    .pipe(gulp.dest("dist"))
+}
+function _jade(){
+  return gulp.src("*.jade")
+    .pipe(jade({"pretty":true}))
+    .pipe(gulp.dest("dist"))
+}
+function _js(){
+  return gulp.src("*.js")
+    .pipe(gulp.dest("dist"))
+}
+function _transpile(){//DOESNT WORK
+  return gulp.src("*.js")
+    .pipe(gulp.dest("dist"))
+}
+function _minify(){//DOESNT WORK
+  return gulp.src("blog.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("dist"))
+}
+function _lint(){
+  return gulp.src("*.js")
+    .pipe(jshint({"esversion":6}))
+    .pipe(jshint.reporter('default'))
+    .pipe(gulp.dest("dist"))
+}
+function watch(bs){
+  gulp.watch("*.html", _html)
+  gulp.watch("*.js", _js)
+  //gulp.watch("dist/*.html").on('change', bs.reload)
+}
+function polyfill(){
+  return gulp.src("node_modules/babel-polyfill/browser.js")
+    .pipe(gulp.dest("dist/node_modules/babel-polyfill"))
+}
+
+function _bs(){
+  return bs.init({server: {"baseDir": "./dist/"}})
+}
+function browserSync(){
+  bs.init({
+    server: './dist',
+    port: 8080,
+    ui: {
+      port: 8081
+    }
+  });
+}
+
+module.exports = {_html, watch, polyfill, _js, _bs,
+  default: gulp.series(_html, _jade, _js, _lint, _minify, polyfill, watch)
+}
